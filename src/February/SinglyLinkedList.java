024febb1cc854951815bbf5c0ae6cb8b7ed2bd61
Aditya Verma <vermaaditya646@gmail.com>
2021-02-28 19:36:53 +0530

    create method insertAtEnd()

--- src/February/SinglyLinkedList.java
+++ src/February/SinglyLinkedList.java
@@ -6,4 +6,16 @@ public class SinglyLinkedList {
     public SinglyLinkedList(Node head) {
         this.head = null;
     }
+
+    public void insertAtEnd(Node newNode){
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        Node temp= head;
+        while(temp!=null){
+            temp=temp.next;
+        }
+        temp=newNode;
+    }
 }
