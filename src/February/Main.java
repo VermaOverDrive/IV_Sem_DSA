e3ec0578ab3f972d1d1aca06d6abcb32696dcc65
Aditya Verma <vermaaditya646@gmail.com>
2021-02-28 20:08:26 +0530

    final commit

--- src/February/Main.java
+++ src/February/Main.java
@@ -1,4 +1,12 @@
 package February;
 
 public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList LL=new SinglyLinkedList();
+        LL.insertAtEnd(new Node( 5));
+        LL.insertAtEnd(new Node( 6));
+        LL.insertAtEnd(new Node(9));
+        LL.insertAtEnd(new Node(10));
+        LL.print();
+    }
 }
