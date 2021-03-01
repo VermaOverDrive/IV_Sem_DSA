43ec1e5d173aa35422c8ac6fbf442f426cf62898
Aditya Verma <vermaaditya646@gmail.com>
2021-03-02 01:18:25 +0530

    correct the code of insertATMiddle

--- src/February/Main.java
+++ src/February/Main.java
@@ -8,12 +8,12 @@ public class Main {
         LL.insertAtEnd(new Node( 6));
         LL.insertAtEnd(new Node(9));
         LL.insertAtEnd(new Node(10));
+        LL.insertAtEnd(new Node(11));
         LL.print();
         LL.insertAtBegin(new Node(8 ));
         LL.print();
         LL.insertAtMiddle(new Node(3));
-        LL.print();
-        LL.insertAtMiddle(new Node(888));
+
 
         LL.print();
 
