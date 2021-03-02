1884184bc14304bba2ed4a05a1d2d3d3e7a62638
Aditya Verma <vermaaditya646@gmail.com>
2021-03-03 01:23:21 +0530

    create constructor

---
+++ src/March/m_03/DoublyLinkedList.java
@@ -0,0 +1,29 @@
+package March.m_03;
+
+public class DoublyLinkedList {
+    public Node head;
+    public Node tail;
+
+
+    public DoublyLinkedList()
+    {this.head=null;
+     this.tail=null;
+    }
+    public void insertAtEnd(Node newNode){
+        if (head==null){
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
