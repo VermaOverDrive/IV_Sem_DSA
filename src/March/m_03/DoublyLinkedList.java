ed9ab03046053b026647a92e7e14c6f13b21529c
Aditya Verma <vermaaditya646@gmail.com>
2021-03-03 01:37:01 +0530

    final commit

--- src/March/m_03/DoublyLinkedList.java
+++ src/March/m_03/DoublyLinkedList.java
@@ -5,23 +5,21 @@ public class DoublyLinkedList {
     public Node tail;
 
 
-    public DoublyLinkedList()
-    {this.head=null;
-     this.tail=null;
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
     }
-    public void insertAtEnd(Node newNode){
-        if (head==null){
 
-
-
-
-
-
-
-
-
-
+    public void insertAtEnd(Node newNode) {
+        if (head == null) {
+                head=tail=newNode;
         }
+        else
+            tail.next=newNode;
+            newNode.prev=tail;
+            tail=tail.next;
+
+
 
 
 
