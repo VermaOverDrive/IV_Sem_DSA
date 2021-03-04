07665988d194c2126fe1d61042c0ba3e916dbae2
Aditya Verma <vermaaditya646@gmail.com>
2021-03-05 02:51:27 +0530

    method empty and full is implemented

--- src/March/m_04/stackUsingArray.java
+++ src/March/m_04/stackUsingArray.java
@@ -1,33 +1,58 @@
 package March.m_04;
 
 public class stackUsingArray {
-    int top ;
-    int[]arr;
+    int top;
+    int[] arr;
 
-    public stackUsingArray(int size){
-        arr=new int[size];
-        this.top=-1;
+    public stackUsingArray(int size) {
+        arr = new int[size];
+        this.top = -1;
 
     }
 
-    public void push(int data){
-        if(top==arr.length-1){
-            System.out.println("Stack is null");
-        }
-        else{
+    public void push(int data) {
+        if (top == arr.length - 1) {
+            System.out.println("Stack is Full");
+        } else {
             top++;
-            arr[top]=data;
+            arr[top] = data;
 
         }
     }
-    public void pop(){
-        if (top==-1){
+
+    public void pop() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+
+        } else {
+            System.out.println(arr[top]);
+            top--;
+        }
+    }
+
+    public void peek() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+        } else {
+            System.out.println(arr[top]);
+        }
+    }
+
+    public void full() {
+        if (top == arr.length - 1) {
+            System.out.println("Stack is full");
+        } else {
+            System.out.println("Stack is not full");
+        }
+    }
+
+    public void empty(){
+        if(top==-1){
             System.out.println("Stack is empty");
 
         }
         else{
-            System.out.println(arr[top]);
-            top-- ;
+            System.out.println("Stack is not empty");
         }
     }
 
