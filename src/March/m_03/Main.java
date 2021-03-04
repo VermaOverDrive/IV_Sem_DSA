3cb464d56cee466b74c5e1e17d6fac6bca8499d0
Aditya Verma <vermaaditya646@gmail.com>
2021-03-04 16:31:49 +0530

    final commit

--- src/March/m_03/Main.java
+++ src/March/m_03/Main.java
@@ -3,13 +3,8 @@ package March.m_03;
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedList dd=new DoublyLinkedList() ;
-        dd.insertAtEnd(new Node (9));
-        dd.insertAtEnd(new Node(7));
         dd.print();
-        dd.insertAtEnd(new Node(99));
-        dd.print();
-        dd.insertAtBegin(new Node(88));
-        dd.print();
+        System.out.println(dd.search(new Node(2)));
 
     }
 }
