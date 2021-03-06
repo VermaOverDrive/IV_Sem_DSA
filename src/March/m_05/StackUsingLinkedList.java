33cd971d0ecd2c3b3a90a2096e9292fb7fd97d5a
Aditya Verma <vermaaditya646@gmail.com>
2021-03-06 19:17:01 +0530

    final commit

--- src/March/m_05/StackUsingLinkedList.java
+++ src/March/m_05/StackUsingLinkedList.java
@@ -1,5 +1,35 @@
 package March.m_05;
 
 public class StackUsingLinkedList {
+    Node top;
+
+    public StackUsingLinkedList() {
+        this.top = null;
+    }
+
+    public void push(Node newNode) {
+        if (top == null) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+
+    }
+
+    public void pop() {
+        if (top == null) {
+            return;
+        }
+        top=top.next;
+    }
+
+    public void peek(){
+        if(top==null){
+            return;
+        }
+        System.out.println(top.data);
+    }
 
 }
+
